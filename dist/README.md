99041d0dbe40e8c03190eab5cf8dee58eeed032b
wjk <1159085983@qq.com>
2019-04-25 15:08:53 +0800

    Add 'dist/' from commit '34a6b6021d2bd4fb03a4bff43f7e7edd1cd0d0da'
    git-subtree-dir: dist
    git-subtree-mainline: 34a6b6021d2bd4fb03a4bff43f7e7edd1cd0d0da
    git-subtree-split: 34a6b6021d2bd4fb03a4bff43f7e7edd1cd0d0da

---
+++ dist/README.md
@@ -0,0 +1,36 @@
+# webpack 4 and babel 7 简单的vue项目
+
+``` bash
+# 支持jsx写法
+
+# 支持dll
+
+# 支持externals
+
+```
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# 如有dll预打包
+npm run dll
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+```
+
+# 注意点：
+
+``` bash
+dll 打包 vue， vendor里写成vue/dist/vue.js
+简单版本不多说明，东西比较少，但是比较实用
+```
---
+++ dist/build/webpack.base.conf.js
@@ -0,0 +1,46 @@
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const vueLoaders = require('./vue.loaders')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const config = require('../config')
+
+module.exports = {
+  context: path.resolve(__dirname, '../'),
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.js',
+      '@': utils.resolve('src'),
+      'vendor': path.resolve(__dirname, '../src/vendor')
+    }
+  },
+  module: {
+    rules: vueLoaders
+  },
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    setImmediate: false,
+    // prevent webpack from injecting mocks to Node native modules
+    // that does not make sense for the client
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
